557773e6b62aae6be5c9eb4f1adf7a022a8813a9
niikawa naoto <nfami8naoto@gmail.com>
2024-01-17 14:54:41 +0900

    API以外終了

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -32,7 +32,7 @@ class ReservationController extends Controller
     {
         $infos = CakeInfo::all();
         $today = Carbon::today();
-         //今日以降のものを抽出→日にち順に並び変える→時間順に並び変える→ゲット
+        //今日以降のものを抽出→日にち順に並び変える→時間順に並び変える→ゲット
         $reservations = Main_reservation::whereDate('birthday', $today)->get();
         $date = Carbon::today()->format('Y年m月d日');
 
@@ -89,14 +89,14 @@ class ReservationController extends Controller
         $infos = CakeInfo::all();
         $cakename = $cakeinfo->cakename;
         $reservations = Sub_reservation::where('cakename', $cakename)->get();
-        $count=$reservations->count();
+        $count = $reservations->count();
 
         return view('management.count')
             ->with([
                 'cakeinfos' => $infos,
                 'cakeinfo' => $cakeinfo,
                 'reservations' => $reservations,
-                'count'=>$count,
+                'count' => $count,
             ]);
     }
 
@@ -111,9 +111,16 @@ class ReservationController extends Controller
         // 終わり
         $enddate = $request->enddate;
         //指定した期間内のデータを抽出
-        $info = Main_reservation::whereBetween('birthday',[$startdate,$enddate])->get();
+        $info = Main_reservation::whereHas(
+            'sub_reservations',
+            function ($query) use ($cakename) {
+                $query->where('cakename', $cakename);
+            }
+        )
+            ->whereBetween('birthday', [$startdate, $enddate])
+            ->get();
         //上のデータを指定した商品名のもののみ抽出したい
-        
+
         $count = $info->count();
 
         return view('management.count')
