59535dc15d63c8ad1f68f73a3de65beee221bc76
niikawa naoto <nfami8naoto@gmail.com>
2024-02-16 19:45:51 +0900

    修正

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,30 +1,5 @@
 </main>
 <footer>
-    <ul class=”menus”>
-        @if (Auth::user())
-            <li>ログインされています</li>
-            <li class="">
-                <a href="{{ route('admin.login') }}">管理者としてログイン</a>
-            </li>
-            <li class="">
-                <a href="{{ route('management') }}">管理画面へ</a>
-            </li>
-            <li>
-                <form method="POST" action="/logout">
-                    @csrf
-                    <button>ログアウト</button>
-                </form>
-            </li>
-        @else
-            <li class="">
-                <a href="{{ route('register') }}"> ログイン登録</a>
-            </li>
-            <li class="">
-                <a href="{{ route('login') }}"> ログイン</a>
-            </li>
-        @endif
-
-    </ul>
     <small>©このひとがつくりました</small>
 </footer>
 
