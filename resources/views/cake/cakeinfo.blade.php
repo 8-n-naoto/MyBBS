160c207c9be747876e0e4bfc8a7419eee1a2fd31
niikawa naoto <nfami8naoto@gmail.com>
2024-02-18 20:05:48 +0900

    ファイル切り出し

--- resources/views/cake/cakeinfo.blade.php
+++ resources/views/cake/cakeinfo.blade.php
@@ -13,6 +13,7 @@
 
 @section('js')
     <script src="{{ url('js/arrow-slider.js') }}"></script>
+    <script src="{{ url('js/favoriteButton.js') }}"></script>
 @endsection
 
 @section('main')
@@ -111,4 +112,35 @@
     @include('include.cakes')
 
     @include('include.google-map')
+
+    <script>
+        $('.favorite').on('click', function(e) {
+            e.preventDefault();
+            var user_id=$(this).siblings('#favorite [name="user_id"]').val();
+            var cake_id=$(this).siblings('#favorite [name="cake_id"]').val();
+            var cakeinfos_id=$(this).siblings('#favorite [name="cakeinfos_id"]').val();
+            $.ajax({
+                url: "{{ route('user.favorite.add') }}",
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr('content'),
+                },
+                method: "POST",
+                data: {
+                    "user_id": user_id,
+                    "cake_id": cake_id,
+                },
+                // dataType: "",
+            }).done(function(res) {
+                console.log('通信成功');
+            }).fail(function() {
+                console.log('通信失敗');
+                alert('通信失敗');
+            }).always(function(data) {
+                console.log('実行しました');
+                console.log(user_id);
+                console.log(cake_id);
+
+            });
+        });
+    </script>
 @endsection
